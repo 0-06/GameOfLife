e64b1ba9a6e733a2f59f6645b18d767a12cca2de
0-06 <101843949+0-06@users.noreply.github.com>
2022-05-12 12:16:03 +1200

    Add files via upload

---
+++ goltry2.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class goltry2 here.
+ *
+ * @author Nolan Peterson
+ * @version 12/05/22
+ */
+public class goltry2
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    public final int width = 20;
+    public final int height = 20; 
+    
+    /**
+     * Constructor for objects of class goltry2
+     */
+    public goltry2()
+    {
+        // initialise instance variables
+        x = 0;
+        
+        
+        // Grid
+        public goltry2(){
+            this(100,100);
+        
+        for(int width,int height){
+            this.width = width;
+            this.height = height;
+            this.size = width * height;
+        }
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}

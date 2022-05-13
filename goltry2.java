fd373d123543887ae4291fe61cb9884028b0f2d6
0-06 <101843949+0-06@users.noreply.github.com>
2022-05-13 15:07:04 +1200

    Add files via upload

--- goltry2.java
+++ goltry2.java
@@ -1,46 +1,82 @@
 
 /**
- * Write a description of class goltry2 here.
+ * Line 32 & 57 throwing erors. Commented out.
  *
  * @author Nolan Peterson
- * @version 12/05/22
+ * @version 13/05/22
  */
-public class goltry2
+import java.util.Random;
+import java.util.*;
+
+
+
+ class goltry2
 {
     // instance variables - replace the example below with your own
-    private int x;
-    public final int width = 20;
-    public final int height = 20; 
+   int cellSize = 10; // size of grid square
+        float aliveProbability = 15; // chance of cells being alive when created
+        
+        // array setup
+       int[][] cells;
+       int[][] cellsBuffer;
+       int width = 20;
+       int height = 20;
+      
+       
     
-    /**
-     * Constructor for objects of class goltry2
-     */
-    public goltry2()
-    {
-        // initialise instance variables
-        x = 0;
-        
-        
-        // Grid
-        public goltry2(){
-            this(100,100);
-        
-        for(int width,int height){
-            this.width = width;
-            this.height = height;
-            this.size = width * height;
+   
+    
+  
+       
+        void setup () { // Screen size + Array initilizing
+          // size (20,20);
+           cells = new int[width/cellSize][height/cellSize]; 
+           cellsBuffer = new int[width/cellSize][height/cellSize];
+          
+           
+           
+            // random functions
+            Random rand = new Random();
+      int upperbound = 100;
+
+      float float_random=rand.nextFloat();
+           
+           // Determines which cells are alive or dead on generation
+           
+           for (int x=0; x<width/cellSize; x++){
+               for (int y=0; y<height/cellSize; y++) {
+                   float state = float_random;
+                   if (state > aliveProbability){
+                       state = 0;
+                    }
+                    else {
+                        state = 1;
+                    }
+                    //cells[x][y] = int(state);
+                }
+               
+            }
+            
+          // background(0);
+        }
+void iteration(){
+    for (int x=0; x<width/cellSize; x++){
+        for (int y=0;y<height/cellSize;y++){
+            cellsBuffer[x][y] = cells[x][y];
         }
     }
 
+
+   
+ 
+ 
+}
+}
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+    
+

9af216e0a7d67268bbc2fb9963cc40992b026f22
0-06 <101843949+0-06@users.noreply.github.com>
2022-05-25 10:42:27 +1200

    Add files via upload

--- goltry2.java
+++ goltry2.java
@@ -3,130 +3,106 @@
  * 
  *
  * @author Nolan Peterson
- * @version 19/05/22
+ * @version 20/05/22
  */
 import java.util.Random;
 import java.util.*;
 
-
-
- class goltry2
+class goltry2
 {
-   
-   
-        int aliveProbability = 15; // chance of cells being alive when created
-        
-        // array setup
-       int[][] cells;
-       int[][] cellsBuffer;
-       int width = 20;
-       int height = 20;
-      
-       
-    
-   
-    
-  
-       
-        void setup(String[]args) { // Screen size + Array initilizing
-          
-           cells = new int[width][height]; 
-       
-          
-           
-           
-            // random function
-           
-      Random r = new Random();
 
-     
-      
-          
-           // Determines which cells are alive or dead on generation
-           
-           for (int x=0; x<width; x++){
-               for (int y=0; y<height; y++) {
-                   int state = r.nextInt(100)+1;
-                   if (state > aliveProbability){
-                       state = 0;
-                    }
-                    else {
-                        state = 1;
-                    }
-                    
-                    cells[x][y] = state;
-                    if (cells[x][y] == 0)
-                    
-                 
-                    
-                         System.out.print("  ");
-                    
-                          if (cells[x][y] == 1)
-                         System.out.print("X ");
-                    
-                   
+   final int aliveProbability = 5; // chance of cells being alive when created
+    // array setup
+    int[][] cells; // grid
+    int[][] cellsBuffer;
+    final int width = 20;
+    final int height = 20;
+
+   
+   public void main() { // Screen size + Array initilizing
+        cells = new int[width][height]; 
+        cellsBuffer = new int [width][height];
+
+        // random function
+        Random r = new Random();
+
+        // Determines which cells are alive or dead on generation
+        for (int x=0; x<width; x++){
+            for (int y=0; y<height; y++) {
+                int state = r.nextInt(100)+1;
+                if (state > aliveProbability){
+                    state = 0; // dead
                 }
-                
-                
-                System.out.println();
-               
+                else {
+                    state = 1; // alive
+                }
+
+                cells[x][y] = state;
+                if (cells[x][y] == 0)
+
+                    System.out.print(" ");
+                if (cells[x][y] == 1)
+                    System.out.print("0");
+
             }
-            
-          
+
+            System.out.println();
+
         }
-void iteration(int cells[][], int width, int height){ // Setting up the 'Next' Grid
-    
+        // Next Grid
+          System.out.println("Next Turn");
+ //for (int x=0; x<width; x++){
+            //for (int y=0; y<height; y++) {
+           //     cellsBuffer[x][y] = cells[x][y];
+//
+      //   //   }
+      //  }
 
+        for (int x=0; x<width; x++) {
+            for (int y=0; y<height; y++){
+                int neighbours=0;
 
-   for (int x=0; x<width; x++){
-               for (int y=0; y<height; y++) {
-                   cellsBuffer[x][y] = cells[x][y];
-                   
-                }
-             
-            }
-            
-            for (int x=0; x<width; x++) {
-                for (int y=0; y<height; y++){
-                    int neighbours=0;
-                    for (int xx=x-1; xx<=x+1; xx++){ // Very messy.
-                        for (int yy=y-1; yy<=y+1; yy++){
-                            if (((xx>=0)&&(xx<width))&&((yy>=0)&&(yy<height)))
-                            {
-                                if (!((xx==x)&&(yy==y))){  // Neighbour Check 
-                                    if (cellsBuffer[xx][yy]==1){
-                                        neighbours++;
-                                    }
+                for (int xx=x-1; xx<=x+1; xx++){ // Very messy.
+                    for (int yy=y-1; yy<=y+1; yy++){
+                        if (((xx>=0 && xx<width))&&((yy>=0)&&(yy<height)))
+                        {
+                            if (!((xx==x)&&(yy==y))){  // Neighbour Check 
+                                if (cells[x][y]==1){
+                                    neighbours++;
                                 }
                             }
-                            }
                         }
-                        // More game rules - If less than 2 or more than 3 neighbours, die, if 3 neighbours, live
-                    if(cellsBuffer[x][y]==1){
-                        if (neighbours < 2 || neighbours > 3) {
-                            cells[x][y] = 0;
-                        }
-                    }
-                    else { 
-                        if (neighbours == 3) {
-                            cells[x][y] =1;
-                        }
-                        if (cells[x][y] == 0)
-                    
-                 
-                    
-                         System.out.print("  ");
-                    
-                          if (cells[x][y] == 1)
-                         System.out.print("X ");
-                    }
-                    }
-                    }
-                    
                     }
                 }
+                // More game rules 
+                if(cellsBuffer[x][y]==1){
+                    if ((neighbours < 2) || (neighbours > 3)) {
+                        cells[x][y] = 0;
+                    }
+                }
+                else { 
+                    if (neighbours == 3) {
+                        cells[x][y] =1;
+                    }
+
+                        if (cells[x][y] == 0)
+
+                        System.out.print(" ");
+                    if (cells[x][y] == 1)
+                        System.out.print("0");
+                       
+                }
+                
+            }
+                 System.out.println();       
+        }
             
-            
+        }
+         
+        
+    }
 
 
     
+    
+   

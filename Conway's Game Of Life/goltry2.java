54370cbce0882dd1bf7962fb2b43ffb9290541c6
0-06 <101843949+0-06@users.noreply.github.com>
2022-06-02 12:19:16 +1200

    Add files via upload

--- Conway's Game Of Life/goltry2.java
+++ Conway's Game Of Life/goltry2.java
@@ -6,20 +6,22 @@
  * @version 27/05/22
  */
 import java.util.Random;
-
+  import java.util.Timer; 
 import java.util.Scanner;
-public class goltry2
+import java.util.TimerTask; 
+
+ public class goltry2
 {
 
-    static String deadCell;
-    static  String aliveCell;
-    static int[][] cells; // grid
+  
+    //static int[][] cells; // grid
      static final int WIDTH = 22;
     static final int HEIGHT = 22;
+   static int [][] test;
    
-    
-
-   
+ static int [][]cells;
+    static String deadCell;
+    static  String aliveCell;
     // array setup
     
     
@@ -29,15 +31,15 @@ public class goltry2
      int turnNumber = 1;
      boolean auto = false;
     cells = new int [WIDTH][HEIGHT];
-
+Timer timer = new Timer();
         // Keyboard Scanner
         Scanner keyboard = new Scanner(System.in);
 
         // Setting variables
         System.out.println("What would you like the dead cells to look like?");
-        String deadCell =keyboard.nextLine();
+         deadCell =keyboard.nextLine();
         System.out.println("What would you like the alive cells to look like?");
-        String aliveCell =keyboard.nextLine();
+         aliveCell =keyboard.nextLine();
 
         System.out.println("How likely would you like the cells to be alive on generation? (e.g. 5 for a 5% chance)");
         int aliveProbability = keyboard.nextInt(); // chance of cells being alive when created
@@ -46,8 +48,8 @@ public class goltry2
         Random r = new Random();
 
         // Determines which cells are alive or dead on generation
-        for (int x=1; x<WIDTH; x++){
-            for (int y=1; y<HEIGHT; y++) { // Creating phantom cells that aren't printed to prevent crashes
+        for (int x=1; x<WIDTH-1; x++){
+            for (int y=1; y<HEIGHT-1; y++) { // Creating phantom cells that aren't printed to prevent crashes
                 int state = r.nextInt(100)+1;
                 if (state > aliveProbability){
                     state = 0; // dead
@@ -63,34 +65,43 @@ public class goltry2
                     System.out.print(deadCell+ " ");
                 if (cells[x][y] == 1)
                     System.out.print(aliveCell+ " ");
-
+                   
             }
-
+            
             System.out.println();
 
         }
-        // Next Grid
+        System.out.println();
+        // User interaction switch statement
+        
         System.out.println("Type 'Next' to advance");
         while (quit == false){
             command=keyboard.nextLine();
             switch(command){
                 case "auto":  auto = true;
-                while (auto == true) { }
+                while (auto == true) 
+                setInterval(nextGen(), 1000);
+                
+                break;
+                case "stop": auto = false;
+                break;
                 case "next":case "Next":
 
                 System.out.println("Turn #"+turnNumber);
                 turnNumber++;
                 nextGen();
+                break;
 
             }
         }
     }
 
     public static void nextGen(){
-        for (int x=1; x<WIDTH; x++) {
-            for (int y=1; y<HEIGHT; y++){ // Creating phantom cells that aren't printed to prevent crashes
+       
+         for (int x=1; x<WIDTH-1; x++){
+            for (int y=1; y<HEIGHT-1; y++) {// Creating phantom cells that aren't printed to prevent crashes
                 int neighbours=0;
-                cells = new int [x][y];
+                
                 //*** Neighbour check and bump 
 
                 if (cells[x][y+1]==1) {

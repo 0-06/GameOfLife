5d5c5e7487f6c4145423564ad0bf101d6c3455db
0-06 <101843949+0-06@users.noreply.github.com>
2022-06-23 12:14:38 +1200

    Add files via upload

--- Conway's Game Of Life/goltry2.java
+++ Conway's Game Of Life/goltry2.java
@@ -20,13 +20,15 @@ public class goltry2
     static int [][]future;
     static boolean auto = false; // Automatically printing
     static boolean sameGrid =false;
+    static String chooseMode;
     // array setup
 
     public static void main(String[] args) { 
         String command; // While quit is false, we will be overwriting this string loads
         boolean quit = false; // Looking for a 'command'
         int turnNumber = 1; // Amount of turns is 1
-
+        int row;
+        int column;
         int autoDuration = 1; // How long  it has been auto
         int autoNum = 0; // How many times has it will print
         cells = new int [WIDTH][HEIGHT]; 
@@ -40,171 +42,200 @@ public class goltry2
         System.out.println("What would you like the alive cells to look like?");
         aliveCell =keyboard.nextLine();
 
-        System.out.println("How likely would you like the cells to be alive on generation? (e.g. 5 for a 5% chance)");
-        int aliveProbability = keyboard.nextInt(); // chance of cells being alive when created
-
         // random function
         Random r = new Random();
 
         // Determines which cells are alive or dead on generation
-        for (int x=1; x<WIDTH-1; x++){
-            for (int y=1; y<HEIGHT-1; y++) { // Creating phantom cells that aren't printed to prevent crashes
-                int state = r.nextInt(100)+1; // State = a random number 1-100
-                if (state > aliveProbability){ // If state is greater than alive probability, die
-                    state = 0; // dead
-                }
-                else {
-                    state = 1; // alive
+        System.out.println("Would you like cells to be automatically generated or manually?");
+        chooseMode=keyboard.nextLine();
+        chooseMode=chooseMode.toLowerCase();
+
+        switch(chooseMode){ // Choosing auto or manual generation
+            case "auto": case"1": case "automatic":
+            System.out.println("You have chosen automatic");
+            System.out.println("How likely would you like the cells to be alive on generation? (e.g. 5 for a 5% chance)");
+            int aliveProbability = keyboard.nextInt(); // chance of cells being alive when created
+            for (int x=1; x<WIDTH-1; x++){
+                for (int y=1; y<HEIGHT-1; y++) { // Creating phantom cells that aren't printed to prevent crashes
+                    int state = r.nextInt(100)+1; // State = a random number 1-100
+                    if (state > aliveProbability){ // If state is greater than alive probability, die
+                        state = 0; // dead
+                    }
+                    else {
+                        state = 1; // alive
+                    }
+
+                    cells[x][y] = state; // print dead/alive cells
+                    if (cells[x][y] == 0)
+
+                        System.out.print(deadCell+ " "); 
+                    if (cells[x][y] == 1)
+                        System.out.print(aliveCell+ " ");
+
                 }
 
-                cells[x][y] = state; // print dead/alive cells
-                if (cells[x][y] == 0)
+                System.out.println();
+
+            }
+
+            System.out.println();
+            break; 
+
+            case "manual": case "2": 
+            System.out.println("You have chosen manual");
+            System.out.println("Select the row of a cell you would like to turn on");
+            row=keyboard.nextInt();
+            System.out.println("Select the column of a cell you would like to turn on");
+            column=keyboard.nextInt();
+            for (int x=1; x<WIDTH-1; x++){
+                for (int y=1; y<HEIGHT-1; y++) {
+
+                    cells[row][column]=1;
+                    
+                    if (cells[row][column] == 0)
 
                     System.out.print(deadCell+ " "); 
-                if (cells[x][y] == 1)
-                    System.out.print(aliveCell+ " ");
-
-            }
-
-            System.out.println();
-
-        }
-        System.out.println();
-        // User interaction switch statement
-
-        System.out.println("Type 'Next' to advance");
-        while (!quit){ // While the user has not quit, always look for a command
-            command=keyboard.nextLine();
-            command=command.toLowerCase(); // Converts input into lowercase, meaning commands are not case sensitive
-            switch(command){
-                // If the command is automatic, ask for how many turns to advance
-                case "auto":  auto = true;
-                System.out.println("How many turns would you like it to be automatic for?");
-                autoNum = keyboard.nextInt(); // how many grids to auto print
-                while (auto == true){ //While auto is true, and autoduration is less than autonum, print grids at a 250ms interval
-
-                    try {
-                        Thread.sleep(250);
-                        System.out.println("Turn #"+turnNumber);
-
-                        nextGen();
-                        turnNumber++;
-                        autoDuration++;
-                    } catch (Exception e) {
-
-                    }
-                    if (autoDuration > autoNum) { // if it's been going too long, change auto to false, and autoduration to 1
-                        auto=false;
-                        autoDuration=1;
-                    } 
-                    for (int x=1; x<WIDTH-1; x++){
-                        for (int y=1; y<HEIGHT-1; y++)
-                            if (Arrays.deepEquals(future,cells)) {
-                               auto=false;
-                               autoDuration=1;
-                            }
-                            
-
-                        
-                    }
-                }
-
-                break;
-                case "next": // Advances 1 turn
-                System.out.println("Turn #"+turnNumber);
-                turnNumber++;
-                nextGen();
-                break;
-                case "quit": quit = true; // Stops asking for keyboard input
-                break;
-                case "": 
-                break;
-                default: 
-                System.out.println("Invalid Command");
-                break;
-
-            }
-        } keyboard.close();
-    }
-
-    public static void nextGen(){
-        int [][]future= new int [WIDTH][HEIGHT];
-        for (int x=1; x<WIDTH-1; x++){
-            for (int y=1; y<HEIGHT-1; y++) {// Creating phantom cells that aren't printed to prevent crashes
-                int neighbours=0;
-
-                //*** Neighbour check and bump. Should be tidied up
-
-                if (cells[x][y+1]==1) {
-                    neighbours++;
-                }
-                if (cells[x][y-1]==1) {
-                    neighbours++;
-                }
-                if (cells[x+1][y+1]==1) {
-                    neighbours++;
-                }
-                if (cells[x+1][y-1]==1) {
-                    neighbours++;
-                }
-                if (cells[x+1][y]==1) {
-                    neighbours++;
-                }
-                if (cells[x-1][y+1]==1) {
-                    neighbours++;
-                }
-                if (cells[x-1][y]==1) {
-                    neighbours++;
-                }
-                if (cells[x-1][y-1]==1) {
-                    neighbours++;
-                }
-
-
-                //  game rules 
-                if(cells[x][y]==1){
-                    if ((neighbours < 2) || (neighbours > 3)) { // If a cell has less than 2 or more than 3 neighbours, Die
-                        future[x][y] = 0;
-                    }
-                }
-
-                if (cells[x][y]==1 && neighbours ==2 ){
-                    future[x][y]=1;
-                }
-
-                if (cells[x][y]==0 && neighbours == 2){
-                    future[x][y]=0;
-                }
-
-                if (neighbours == 3) { // If a cell has 3 neighbours, become a live cell.
-                    future[x][y] =1;
-                }
-                // If not, remain the same
-
-                // Print grid
-                if (future[x][y] == 0){
-
-                    System.out.print(deadCell+" ");
-                }
-                if (future[x][y] == 1){
-
+                if (cells[row][column] == 1)
                     System.out.print(aliveCell+ " ");
                 }
+                 System.out.println();
+
+            }
+           
+            break;
+        }
+    
+
+    // User interaction switch statement
+
+    System.out.println("Type 'Next' to advance");
+    while (!quit){ // While the user has not quit, always look for a command
+        command=keyboard.nextLine();
+        command=command.toLowerCase(); // Converts input into lowercase, meaning commands are not case sensitive
+        switch(command){
+            // If the command is automatic, ask for how many turns to advance
+            case "auto":  auto = true;
+            System.out.println("How many turns would you like it to be automatic for?");
+            autoNum = keyboard.nextInt(); // how many grids to auto print
+            while (auto == true){ //While auto is true, and autoduration is less than autonum, print grids at a 250ms interval
+
+                try {
+                    Thread.sleep(250);
+                    System.out.println("Turn #"+turnNumber);
+
+                    nextGen();
+                    turnNumber++;
+                    autoDuration++;
+                } catch (Exception e) {
+
+                }
+                if (autoDuration > autoNum) { // if it's been going too long, change auto to false, and autoduration to 1
+                    auto=false;
+                    autoDuration=1;
+                } 
+
+                if (cells==future) {
+                    auto=false;
+                    autoDuration=1;
+                }
 
             }
 
-            System.out.println();
+            break;
+            case "next": // Advances 1 turn
+            System.out.println("Turn #"+turnNumber);
+            turnNumber++;
+            nextGen();
+            break;
+            case "quit": quit = true; // Stops asking for keyboard input
+            break;
+            case "": 
+            break;
+            default: 
+            System.out.println("Invalid Command");
+            break;
 
         }
+    } keyboard.close();
+}
 
-        for (int x=1; x<WIDTH-1; x++){
-            for (int y=1; y<HEIGHT-1; y++)
-                if (future[x][y]==1) {
-                    cells[x][y]=1;
-                }
-                else{
-                    cells[x][y]=0;
-                }
-        }
-    }
+public static void nextGen(){
+int [][]future= new int [WIDTH][HEIGHT];
+for (int x=1; x<WIDTH-1; x++){
+for (int y=1; y<HEIGHT-1; y++) {// Creating phantom cells that aren't printed to prevent crashes
+int neighbours=0;
+
+//*** Neighbour check and bump. Should be tidied up
+
+if (cells[x][y+1]==1) {
+neighbours++;
+}
+if (cells[x][y-1]==1) {
+neighbours++;
+}
+if (cells[x+1][y+1]==1) {
+neighbours++;
+}
+if (cells[x+1][y-1]==1) {
+neighbours++;
+}
+if (cells[x+1][y]==1) {
+neighbours++;
+}
+if (cells[x-1][y+1]==1) {
+neighbours++;
+}
+if (cells[x-1][y]==1) {
+neighbours++;
+}
+if (cells[x-1][y-1]==1) {
+neighbours++;
+}
+
+//  game rules 
+if(cells[x][y]==1){
+if ((neighbours < 2) || (neighbours > 3)) { // If a cell has less than 2 or more than 3 neighbours, Die
+future[x][y] = 0;
+}
+}
+
+if (cells[x][y]==1 && neighbours ==2 ){
+future[x][y]=1;
+}
+
+if (cells[x][y]==0 && neighbours == 2){
+future[x][y]=0;
+}
+
+if (neighbours == 3) { // If a cell has 3 neighbours, become a live cell.
+future[x][y] =1;
+}
+// If not, remain the same
+
+// Print grid
+if (future[x][y] == 0){
+
+System.out.print(deadCell+" ");
+}
+if (future[x][y] == 1){
+
+System.out.print(aliveCell+ " ");
+}
+
+}
+
+System.out.println();
+
+}
+
+for (int x=1; x<WIDTH-1; x++){
+for (int y=1; y<HEIGHT-1; y++)
+if (future[x][y]==1) {
+cells[x][y]=1;
+}
+else{
+cells[x][y]=0;
+}
+}
+}
 }

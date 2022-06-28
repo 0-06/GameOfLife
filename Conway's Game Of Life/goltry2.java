1434d66cb66017203fee6de17b7b8b8889da3b21
0-06 <101843949+0-06@users.noreply.github.com>
2022-06-29 11:02:17 +1200

    Add files via upload

--- Conway's Game Of Life/goltry2.java
+++ Conway's Game Of Life/goltry2.java
@@ -27,8 +27,8 @@ public class goltry2
         String command; // While quit is false, we will be overwriting this string loads
         boolean quit = false; // Looking for a 'command'
         int turnNumber = 1; // Amount of turns is 1
-        int row;
-        int column;
+        int row = 0;
+        int column = 0;
         int autoDuration = 1; // How long  it has been auto
         int autoNum = 0; // How many times has it will print
         cells = new int [WIDTH][HEIGHT]; 
@@ -54,6 +54,7 @@ public class goltry2
             case "auto": case"1": case "automatic":
             System.out.println("You have chosen automatic");
             System.out.println("How likely would you like the cells to be alive on generation? (e.g. 5 for a 5% chance)");
+
             int aliveProbability = keyboard.nextInt(); // chance of cells being alive when created
             for (int x=1; x<WIDTH-1; x++){
                 for (int y=1; y<HEIGHT-1; y++) { // Creating phantom cells that aren't printed to prevent crashes
@@ -81,161 +82,182 @@ public class goltry2
             System.out.println();
             break; 
 
-            case "manual": case "2": 
-            System.out.println("You have chosen manual");
-            System.out.println("Select the row of a cell you would like to turn on");
-            row=keyboard.nextInt();
-            System.out.println("Select the column of a cell you would like to turn on");
-            column=keyboard.nextInt();
-            for (int x=1; x<WIDTH-1; x++){
-                for (int y=1; y<HEIGHT-1; y++) {
+            case "manual": case "2": case "manually":case"man":
+            System.out.println("You have chosen manual, Type any word when you would like to continue");
+            boolean manualPlaying = true;
+            while (manualPlaying){
 
-                    cells[row][column]=1;
-                    
-                    if (cells[row][column] == 0)
+                System.out.println("Select the row of a cell you would like to turn on");
 
-                    System.out.print(deadCell+ " "); 
-                if (cells[row][column] == 1)
+                if(keyboard.hasNextInt()){ // If there is an int, it means they want to change column. If not, they probably want to continue.
+                    row=keyboard.nextInt();
+                }
+                else {
+
+                    chooseMode=keyboard.nextLine();
+                    manualPlaying = false;
+                    break;
+                }
+                System.out.println("Select the column of a cell you would like to turn on");
+                if(keyboard.hasNextInt()){
+                    column=keyboard.nextInt();
+                }
+                else {
+
+                    chooseMode=keyboard.nextLine();
+                    manualPlaying = false;
+                    break;
+                }
+                for (int x=1; x<WIDTH-1; x++){
+                    for (int y=1; y<HEIGHT-1; y++) {
+
+                        cells[row][column]=1;
+
+                        if (cells[x][y] == 0)
+
+                            System.out.print(deadCell+ " "); 
+                        if (cells[x][y] == 1)
+                            System.out.print(aliveCell+ " ");
+                    }
+                    System.out.println();
+
+                }
+            }
+            break;
+           
+           
+        }
+
+        // User interaction switch statement
+        System.out.println("Type 'Next' to advance");
+        while (!quit){ // While the user has not quit, always look for a command
+            command=keyboard.nextLine();
+            command=command.toLowerCase(); // Converts input into lowercase, meaning commands are not case sensitive
+            switch(command){
+                // If the command is automatic, ask for how many turns to advance
+                case "auto":  auto = true;
+                System.out.println("How many turns would you like it to be automatic for?");
+                autoNum = keyboard.nextInt(); // how many grids to auto print
+                while (auto == true){ //While auto is true, and autoduration is less than autonum, print grids at a 250ms interval
+
+                    try {
+                        Thread.sleep(250);
+                        System.out.println("Turn #"+turnNumber);
+
+                        nextGen();
+                        turnNumber++;
+                        autoDuration++;
+                    } catch (Exception e) {
+
+                    }
+                    if (autoDuration > autoNum) { // if it's been going too long, change auto to false, and autoduration to 1
+                        auto=false;
+                        autoDuration=1;
+                    } 
+
+                    boolean arrayCheck = Arrays.equals(cells,future);
+                    if (arrayCheck==true){
+                        auto=false;
+                        autoDuration=1;
+                    }
+
+                }
+
+                break;
+                case "next": // Advances 1 turn
+                System.out.println("Turn #"+turnNumber);
+                turnNumber++;
+                nextGen();
+                break;
+                case "quit": quit = true; // Stops asking for keyboard input
+                break;
+                case "": 
+                break;
+                default: 
+                System.out.println("Invalid Command");
+                break;
+
+            }
+        } keyboard.close();
+    }
+
+    public static void nextGen(){
+        int [][]future= new int [WIDTH][HEIGHT];
+        for (int x=1; x<WIDTH-1; x++){
+            for (int y=1; y<HEIGHT-1; y++) {// Creating phantom cells that aren't printed to prevent crashes
+                int neighbours=0;
+
+                //*** Neighbour check and bump. Should be tidied up
+
+                if (cells[x][y+1]==1) {
+                    neighbours++;
+                }
+                if (cells[x][y-1]==1) {
+                    neighbours++;
+                }
+                if (cells[x+1][y+1]==1) {
+                    neighbours++;
+                }
+                if (cells[x+1][y-1]==1) {
+                    neighbours++;
+                }
+                if (cells[x+1][y]==1) {
+                    neighbours++;
+                }
+                if (cells[x-1][y+1]==1) {
+                    neighbours++;
+                }
+                if (cells[x-1][y]==1) {
+                    neighbours++;
+                }
+                if (cells[x-1][y-1]==1) {
+                    neighbours++;
+                }
+
+                //  game rules 
+                if(cells[x][y]==1){
+                    if ((neighbours < 2) || (neighbours > 3)) { // If a cell has less than 2 or more than 3 neighbours, Die
+                        future[x][y] = 0;
+                    }
+                }
+
+                if (cells[x][y]==1 && neighbours ==2 ){
+                    future[x][y]=1;
+                }
+
+                if (cells[x][y]==0 && neighbours == 2){
+                    future[x][y]=0;
+                }
+
+                if (neighbours == 3) { // If a cell has 3 neighbours, become a live cell.
+                    future[x][y] =1;
+                }
+                // If not, remain the same
+
+                // Print grid
+                if (future[x][y] == 0){
+
+                    System.out.print(deadCell+" ");
+                }
+                if (future[x][y] == 1){
+
                     System.out.print(aliveCell+ " ");
                 }
-                 System.out.println();
-
-            }
-           
-            break;
-        }
-    
-
-    // User interaction switch statement
-
-    System.out.println("Type 'Next' to advance");
-    while (!quit){ // While the user has not quit, always look for a command
-        command=keyboard.nextLine();
-        command=command.toLowerCase(); // Converts input into lowercase, meaning commands are not case sensitive
-        switch(command){
-            // If the command is automatic, ask for how many turns to advance
-            case "auto":  auto = true;
-            System.out.println("How many turns would you like it to be automatic for?");
-            autoNum = keyboard.nextInt(); // how many grids to auto print
-            while (auto == true){ //While auto is true, and autoduration is less than autonum, print grids at a 250ms interval
-
-                try {
-                    Thread.sleep(250);
-                    System.out.println("Turn #"+turnNumber);
-
-                    nextGen();
-                    turnNumber++;
-                    autoDuration++;
-                } catch (Exception e) {
-
-                }
-                if (autoDuration > autoNum) { // if it's been going too long, change auto to false, and autoduration to 1
-                    auto=false;
-                    autoDuration=1;
-                } 
-
-                if (cells==future) {
-                    auto=false;
-                    autoDuration=1;
-                }
 
             }
 
-            break;
-            case "next": // Advances 1 turn
-            System.out.println("Turn #"+turnNumber);
-            turnNumber++;
-            nextGen();
-            break;
-            case "quit": quit = true; // Stops asking for keyboard input
-            break;
-            case "": 
-            break;
-            default: 
-            System.out.println("Invalid Command");
-            break;
+            System.out.println();
 
         }
-    } keyboard.close();
-}
 
-public static void nextGen(){
-int [][]future= new int [WIDTH][HEIGHT];
-for (int x=1; x<WIDTH-1; x++){
-for (int y=1; y<HEIGHT-1; y++) {// Creating phantom cells that aren't printed to prevent crashes
-int neighbours=0;
-
-//*** Neighbour check and bump. Should be tidied up
-
-if (cells[x][y+1]==1) {
-neighbours++;
-}
-if (cells[x][y-1]==1) {
-neighbours++;
-}
-if (cells[x+1][y+1]==1) {
-neighbours++;
-}
-if (cells[x+1][y-1]==1) {
-neighbours++;
-}
-if (cells[x+1][y]==1) {
-neighbours++;
-}
-if (cells[x-1][y+1]==1) {
-neighbours++;
-}
-if (cells[x-1][y]==1) {
-neighbours++;
-}
-if (cells[x-1][y-1]==1) {
-neighbours++;
-}
-
-//  game rules 
-if(cells[x][y]==1){
-if ((neighbours < 2) || (neighbours > 3)) { // If a cell has less than 2 or more than 3 neighbours, Die
-future[x][y] = 0;
-}
-}
-
-if (cells[x][y]==1 && neighbours ==2 ){
-future[x][y]=1;
-}
-
-if (cells[x][y]==0 && neighbours == 2){
-future[x][y]=0;
-}
-
-if (neighbours == 3) { // If a cell has 3 neighbours, become a live cell.
-future[x][y] =1;
-}
-// If not, remain the same
-
-// Print grid
-if (future[x][y] == 0){
-
-System.out.print(deadCell+" ");
-}
-if (future[x][y] == 1){
-
-System.out.print(aliveCell+ " ");
-}
-
-}
-
-System.out.println();
-
-}
-
-for (int x=1; x<WIDTH-1; x++){
-for (int y=1; y<HEIGHT-1; y++)
-if (future[x][y]==1) {
-cells[x][y]=1;
-}
-else{
-cells[x][y]=0;
-}
-}
-}
+        for (int x=1; x<WIDTH-1; x++){
+            for (int y=1; y<HEIGHT-1; y++)
+                if (future[x][y]==1) {
+                    cells[x][y]=1;
+                }
+                else{
+                    cells[x][y]=0;
+                }
+        }
+    }
 }
